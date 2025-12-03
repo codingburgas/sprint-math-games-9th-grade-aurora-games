6799cac4f70dd78d218090d24d4be9cfd53889d8
nikita yakubskii <NYAkubskii25@codingburgas.bg>
2025-12-03 19:33:10 +0200

    Add utils.cpp file

---
+++ main/utils.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include "utils.h"
+#include "globals.h"
+
+using namespace std;
+
+void wait(double seconds) {
+    int start = clock();
+    while (clock() - start < (seconds * 1000)) {}
+}
+
+void screenClear() {
+    system("cls");
+}
+
+void putData() {
+    string path = "data.txt";
+    ifstream fin;
+    fin.open(path);
+    if (!fin.is_open())
+    {
+        cout << "Error: File can not be opened" << endl;
+    }
+    else
+    {
+        if (getline(fin, language)) {
+            if (getline(fin, difficulty)) {
+                cout << language << endl;
+                cout << difficulty << endl;
+            }
+            else {
+                cout << R"(
+                    +--------------------------------------+
+                    |              ERROR                   |
+                    +--------------------------------------+
+                    |  Only one parameter found:           |
+                    |  )" << language << R"(               |
+                    +--------------------------------------+
+)" << endl;
+            }
+        }
+        else {
+            cout << "Error: File is empty" << endl;
+        }
+    }
+    fin.close();
+}
+
+void getData(short inputLanguage, short inputDifficulty) {
+    string path = "data.txt";
+    ofstream fout;
+    fout.open(path, ios::out);
+
+    if (!fout.is_open())
+    {
+        cout << "Error: File can not be opened" << endl;
+    }
+    else {
+        if (!(cin >> language)) {
+            fout.close();
+            return;
+        }
+
+        if (!(cin >> difficulty)) {
+            fout.close();
+            return;
+        }
+
+        fout << language << endl;
+        fout << difficulty << endl;
+
+        fout.close();
+    }
+}
